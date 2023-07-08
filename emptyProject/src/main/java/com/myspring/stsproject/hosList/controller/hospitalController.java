fbcb11635a6d5d70859fa17f77e849c63c0f7f53
KimValue <63282513+ksj1884@users.noreply.github.com>
2023-07-08 14:24:43 +0900

    hosList 패키지 수정

--- emptyProject/src/main/java/com/myspring/stsproject/hosList/controller/hospitalController.java
+++ emptyProject/src/main/java/com/myspring/stsproject/hosList/controller/hospitalController.java
@@ -3,8 +3,12 @@ package com.myspring.stsproject.hosList.controller;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.myspring.stsproject.hosList.vo.hospitalVO;
+
 public interface hospitalController {
-	public ModelAndView Hospital_detail (HttpServletRequest request, HttpServletResponse response) throws Exception;
+//	public void Hospital_detail (@ModelAttribute("hospitalVO") hospitalVO hospitalVO, HttpServletRequest request, HttpServletResponse response) throws Exception;
+	public ModelAndView Hospital_detail (@ModelAttribute("hospitalVO") hospitalVO hospitalVO, HttpServletRequest request, HttpServletResponse response) throws Exception;
 }
--- emptyProject/src/main/java/com/myspring/stsproject/hosList/dao/hospitalDAOImpl.java
+++ emptyProject/src/main/java/com/myspring/stsproject/hosList/dao/hospitalDAOImpl.java
@@ -1,5 +1,35 @@
 package com.myspring.stsproject.hosList.dao;
 
-public class hospitalDAOImpl {
+import java.util.HashMap;
 
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.stereotype.Repository;
+
+import com.myspring.stsproject.hosList.vo.hospitalVO;
+
+@Repository("hospitalDAO")
+public class hospitalDAOImpl implements hospitalDAO {
+	
+	@Autowired
+	private SqlSession sqlSession;
+
+	@Override
+	public HashMap<String,Object> hosInfo(String hos_code) throws DataAccessException {
+		HashMap<String,Object> hosInfo = sqlSession.selectOne("mapper.hosDetail.hosInfo", hos_code);
+		return hosInfo;
+	}
+	
+	@Override
+	public HashMap<String,Object> hosJoin(String hos_code) throws DataAccessException {
+		HashMap<String,Object> hosJoin = sqlSession.selectOne("mapper.hosDetail.hosJoin", hos_code);
+		return hosJoin;
+	}
+
+	@Override
+	public String hos_tel(String hos_code) throws DataAccessException {
+		String tel = sqlSession.selectOne("mapper.hosDetail.hos_tel", hos_code);
+		return tel;
+	}
 }
