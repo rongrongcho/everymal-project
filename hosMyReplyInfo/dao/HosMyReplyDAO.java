d09a9562e2011589548a0a84cfcd800a30ea02a2
eunbinaa <119823066+eunbinaa@users.noreply.github.com>
2023-07-09 10:14:26 +0900

    hosMyReplyInfo 패키지

---
+++ hosMyReplyInfo/dao/HosMyReplyDAO.java
@@ -0,0 +1,13 @@
+package com.myspring.stsproject.hosMyReplyInfo.dao;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.dao.DataAccessException;
+
+public interface HosMyReplyDAO {
+	public List selectAllReplyList(Map<String, Integer> pagingMap, String hos_id) throws DataAccessException;
+	public int replyCount(String hos_id) throws DataAccessException;
+	public void deleteReply(String[] items)throws DataAccessException;
+	public String selectReply(String a_code)throws DataAccessException;
+}
---
+++ hosMyReplyInfo/service/HosMyReplyService.java
@@ -0,0 +1,12 @@
+package com.myspring.stsproject.hosMyReplyInfo.service;
+
+import java.util.Map;
+
+import org.springframework.dao.DataAccessException;
+
+public interface HosMyReplyService {
+	public Map<String, Integer> listReply(Map<String, Integer> pagingMap, String hos_id) throws DataAccessException;
+	public int replyCount(String hos_id) throws DataAccessException;
+	public void delReply(String[] items) throws DataAccessException;
+	public String viewReply(String a_code)throws DataAccessException;
+}
